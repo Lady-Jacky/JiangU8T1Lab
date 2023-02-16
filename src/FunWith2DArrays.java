46122f4e2b6fe9b1bf4d74958e94d3ca78b9e8b4
1E10 Laptop <sample@bths.edu>
2023-02-16 10:49:40 -0500

    finished

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class FunWith2DArrays {
+public class FunWith2DArrays {
+
+    public static int totalElements(int[][] list) {
+        int total = 0;
+        for(int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i].length; j++) {
+                total++;
+            }
+        }
+        return total;
+    }
+
+    public static void fourCorners(String[][] list) {
+        String one = "";
+        String two = "";
+        String three = "";
+        String four = "";
+        for (int j = 0; j < list.length; j++) {
+            one = list[0][0];
+            two = list[0][list[j].length - 1];
+            three = list[list.length - 1][0];
+            four = list[list.length - 1][list[j].length - 1];
+        }
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(four);
+    }
+
+    public static void swapFrontAndBackRows(String[][] list) {
+        String[] temp = list[list.length-1];
+        list[list.length-1] = list[0];
+        list[0] = temp;
+    }
 }
