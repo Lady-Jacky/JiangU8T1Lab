d3cd760880052bf1769035e1a8e51269fdd4abca
1E10 Laptop <sample@bths.edu>
2023-02-17 10:53:53 -0500

    Finished x2

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -32,4 +32,61 @@ public class FunWith2DArrays {
         list[list.length-1] = list[0];
         list[0] = temp;
     }
+
+    public static void asciiPrinter(String[][] ascii) {
+        for(String[] rows : ascii) {
+            for(String col: rows) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+    public static double average(int[][] arr) {
+        double aver = 0;
+        int count = 0;
+        for(int[] rows : arr) {
+            for(int col: rows) {
+                aver += col;
+                count++;
+            }
+        }
+        return aver/count;
+    }
+
+    public static int[] indexFound(String[][] arr, String target ) {
+        int[] none = {-1, -1};
+        for(int row = 0; row < arr.length; row++) {
+            for(int col = 0; col < arr[row].length; col++) {
+                if(arr[row][col].equals(target)) {
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return none;
+    }
+
+    public static int[][] split(int[][] arr, int rowTarget, int colTarget) {
+        int[][] answer = new int[rowTarget + 1][colTarget + 1];
+        try {
+            for (int row = 0; row <= rowTarget; row++) {
+                for (int col = 0; col <= colTarget; col++) {
+                    answer[row][col] += arr[row][col];
+                }
+            }
+            return answer;
+        } catch (IndexOutOfBoundsException e) {
+            return new int[0][0];
+        }
+    }
+
+    public static int[][] invert(int[][] arr) {
+        int[][] inverted = new int[arr[0].length][arr.length];
+        for(int col = 0; col < arr[0].length; col++) {
+            for (int rows = 0; rows < arr.length; rows++) {
+               inverted[col][rows] += arr[rows][col];
+            }
+        }
+        return inverted;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,98 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        // TEST CODE FOR totalElements
-        int[][] testArr1 = {{5, 2}, {1, 3}, {7, 9}};
-        System.out.println(FunWith2DArrays.totalElements(testArr1));
-        int[][] testArr2 = {{1, 2, 3}, {6, 5, 4}};
-        System.out.println(FunWith2DArrays.totalElements(testArr2));
-        int[][] testArr3 = {{1, 2, 3, 4}, {7, 8, 9, 10}};
-        System.out.println(FunWith2DArrays.totalElements(testArr3));
-        int[][] testArr4 = {{4, 5, 2}, {1, 9, 7}, {8, 10, 13}};
-        System.out.println(FunWith2DArrays.totalElements(testArr4));
-        int[][] testArr5 = {{4, 5, 2, 4, 1}, {1, 9, 7, 8, 2}, {8, 10, 13, 4, 3}};
-        System.out.println(FunWith2DArrays.totalElements(testArr5));
-        int[][] testArr6 = {{4, 5, 2, 4, 8, 9, 10}, {1, 9, 7, 8, 6, 1, 2}};
-        System.out.println(FunWith2DArrays.totalElements(testArr6));
-        int[][] testArr7 = {{4}, {7}, {8}, {2}};
-        System.out.println(FunWith2DArrays.totalElements(testArr7));
-        int[][] testArr8 = {{4}};
-        System.out.println(FunWith2DArrays.totalElements(testArr8));
-        int[][] testArr9 = {{}};
-        System.out.println(FunWith2DArrays.totalElements(testArr9));
-
-
-
+        // TEST CODE FOR split
+        System.out.println("-------test1--------");
+        int[][] testArr6 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] t1 = FunWith2DArrays.split(testArr6, 1, 1);
+        for (int[] row : t1) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------test2--------");
+        int[][] testArr7 = {{10, 11, 12, 13}, {20, 21, 22, 23}, {30, 31, 32, 33}};
+        int[][] t2 = FunWith2DArrays.split(testArr7, 2, 1);
+        for (int[] row : t2) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------test3--------");
+        int[][] testArr8 = {
+                {1, 2, 3, 4, 5, 6},
+                {7, 8, 9, 10, 11, 12},
+                {13, 14, 15, 16, 17, 18},
+                {19, 20, 21, 22, 23, 24}
+        };
+        int[][] t3 = FunWith2DArrays.split(testArr8, 3, 2);
+        for (int[] row : t3) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------test4--------");
+        int[][] t4 = FunWith2DArrays.split(testArr8, 2, 5);
+        for (int[] row : t4) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------test5--------");
+        int[][] t8 = FunWith2DArrays.split(testArr8, 0, 4);
+        for (int[] row : t8) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------test6--------");
+        int[][] t9 = FunWith2DArrays.split(testArr8, 0, 0);
+        for (int[] row : t9) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------test7--------");
+        int[][] t5 = FunWith2DArrays.split(testArr8, 3, 5);
+        for (int[] row : t5) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------test8--------");
+        int[][] t10 = FunWith2DArrays.split(testArr8, 3, 0);
+        for (int[] row : t10) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------test9--------");
+        int[][] t6 = FunWith2DArrays.split(testArr8, 4, 2);
+        for (int[] row : t6) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------test10-------");
+        int[][] t7 = FunWith2DArrays.split(testArr8, 2, 6);
+        for (int[] row : t7) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
 
     }
+
+
 }
+
